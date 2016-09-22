beb7b587bf25cd83cde8209522a667c930f7238c
Jenny Swift <cheezyspaghetti@gmail.com>
2016-09-22 14:38:13 +1000

    Use bootstrap-sass

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,9 @@ elixir(function(mix) {
     mix.version(["css/style.css"]);
 
     //Copy css from node_modules to my css directory
-    mix.copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'resources/assets/css/bootstrap.min.css');
+    // mix.copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'resources/assets/css/bootstrap.min.css');
+    //bootstrap-sass
+    mix.copy('node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss', 'resources/assets/sass/_bootstrap-custom.scss');
     // mix.copy('node_modules/tooltipster/dist/css/tooltipster.bundle.min.css', 'resources/assets/css/tooltipster.min.css');
     mix.copy('node_modules/animate.css/animate.min.css', 'resources/assets/css/animate.min.css');
     mix.copy('node_modules/sweetalert2/dist/sweetalert2.css', 'resources/assets/css/sweetalert2.css');
