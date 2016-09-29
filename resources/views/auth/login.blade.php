5810c6e6dfc5f2e348e11f15788d19e14674080e
Jenny Swift <cheezyspaghetti@gmail.com>
2016-09-29 11:57:02 +1000

    Work on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,19 +40,16 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
+                                <div class="checkbox-container">
+                                    <label for="remember-me">Remember Me</label>
+                                    <input id="remember-me" type="checkbox" name="remember">
                                 </div>
                             </div>
                         </div>
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i> Login
-                                </button>
+                                <button type="submit" class="btn btn-primary">Login</button>
 
                                 <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
                             </div>
