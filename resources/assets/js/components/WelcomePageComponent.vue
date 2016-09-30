ed1d501389cf6e299175d1d8e27b41265f2ee2c0
Jenny Swift <cheezyspaghetti@gmail.com>
2016-09-30 10:32:54 +1000

    Modify welcome page

--- resources/assets/js/components/WelcomePageComponent.vue
+++ resources/assets/js/components/WelcomePageComponent.vue
@@ -4,7 +4,7 @@
         <div class="content">
             <h1>Welcome!</h1>
             <p>Thanks for checking out my work! :)</p>
-            <p>This is just a demo version. Please be aware that if other people use the demo, they may see the data you enter, although the data does get reset regularly.</p>
+            <p>This is just a demo version. Please be aware that if other people use the demo, they may see the data you enter, and vice versa, although the data does get reset regularly.</p>
         </div>
 
     </div>
